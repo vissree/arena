b88ad943ee178a31770ebfdf576f588d66a3c96d
Vishnu Sreekumar <vishnu@pocketmath.com>
2018-03-20 20:15:40 +0800

    Check for both patterns of cnames

--- aws_scripts/populate_delete_record_set.py
+++ aws_scripts/populate_delete_record_set.py
@@ -23,7 +23,22 @@ def add_all_parent_records(result, filter_type):
                 if DEBUG:
                     print("{0} : {1} : {2}".format(name, rtype, value))
                 r53_db.execute_query(query, (name, value, rtype, ttl, weight, set_id))
+
+def add_if_orphan(result):
+    name, value, rtype, ttl, weight, set_id = result
     
+    # Check both example.domain.com and example.domain.com.
+    t_name = value.strip('.')
+
+    query = "SELECT COUNT(*) FROM {table_name} WHERE name=? OR name=?;".format(table_name=table_name).format(table_name=table_name)
+    output = r53_db.execute_query(query, (t_name, t_name+'.'))
+    count, = output[0]
+
+    if not count:
+        query = "INSERT INTO {table_name} (name, value, type, ttl, weight, set_id) VALUES (?, ?, ?, ?, ?, ?);".format(table_name=table_name_to_del)
+        if DEBUG:
+            print("{0} : {1} : {2}".format(name, rtype, value))
+        r53_db.execute_query(query, (name, value, rtype, ttl, weight, set_id))
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description='Generate a simple database of Route53 records')
@@ -31,6 +46,7 @@ if __name__ == "__main__":
     parser.add_argument('--secret-access-key', '-k', dest='secret_access_key', required=False, default=None, help='AWS Secret Access Key')
     parser.add_argument('--table', '-t', dest='table_name', required=False, default='records', help='Table name in the database')
     parser.add_argument('--hosted-zone-id', '-z', dest='hosted_zone_id', required=True, help='Route53 hosted zone ID')
+    parser.add_argument('--domain', '-d', dest='domain', required=True, help='Domain name')
     args = parser.parse_args()
 
     if DEBUG:
@@ -45,8 +61,10 @@ if __name__ == "__main__":
 
     r53_db.initialize_delete_db()  # Create records_to_del table
 
-    query = "SELECT name, value, type, ttl, weight, set_id FROM {table_name} WHERE (type='A' OR type='CNAME') AND alias=0;"
+    query = "SELECT name, value, type, ttl, weight, set_id FROM {table_name} WHERE (type='A' OR type='CNAME') AND alias=0;".format(table_name=table_name)
     results = r53_db.execute_query(query)
+
+    d_regex = r"\S*\." + re.escape(args.domain) + "\.?$"
 
     for result in results:
         name, value, rtype, ttl, weight, set_id = result
@@ -60,5 +78,7 @@ if __name__ == "__main__":
                 add_all_parent_records(result, 'cname')
             elif re.search(r'ip(-\d{1,3}){4}\.ec2\.internal\.?', value):
                 add_all_parent_records(result, 'private_dns')
+            elif re.search(d_regex, value):
+                add_if_orphan(result)
 
     r53_db.close_connection()
